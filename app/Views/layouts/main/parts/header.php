6023e54632cf152248b917fd6e816cf0fb079bec
Marcelo Dênis <marcelodmdo@gmail.com>
2022-01-10 10:18:57 -0300

    header

--- app/Views/layouts/main/parts/header.php
+++ app/Views/layouts/main/parts/header.php
@@ -54,7 +54,7 @@
                             </div>
                         </div>
                     </li> -->
-                    <li class="nav-item dropdown dropdown-large">
+                    <!-- <li class="nav-item dropdown dropdown-large">
                         <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <span class="alert-count">7</span>
                             <i class='bx bx-bell'></i>
                         </a>
@@ -170,7 +170,7 @@
                                 <div class="text-center msg-footer">View All Notifications</div>
                             </a>
                         </div>
-                    </li>
+                    </li> -->
                     <!-- <li class="nav-item dropdown dropdown-large">
                         <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <span class="alert-count">8</span>
                             <i class='bx bx-comment'></i>
@@ -325,23 +325,23 @@
             </div>
             <div class="user-box dropdown">
                 <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="<?= base_url("panel/assets/images/avatars/avatar-2.png")?>" class="user-img" alt="user avatar">
+                    <img src="<?= base_url("panel/assets/images/avatars/u-admin.png")?>" class="user-img" alt="user avatar">
                     <div class="user-info ps-3">
                         <p class="user-name mb-0"><?= $user["name"] ?></p>
                         <p class="designattion mb-0"><?= $user["m"] ?></p>
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
-                    <li><a class="dropdown-item" href="javascript:;"><i class="bx bx-user"></i><span>Profile</span></a>
+                    <li><a class="dropdown-item" href="javascript:;"><i class="bx bx-user"></i><span>Perfil</span></a>
                     </li>
-                    <li><a class="dropdown-item" href="javascript:;"><i class="bx bx-cog"></i><span>Settings</span></a>
+                    <li><a class="dropdown-item" href="javascript:;"><i class="bx bx-cog"></i><span>Configurações</span></a>
                     </li>
                     <li><a class="dropdown-item" href="javascript:;"><i class='bx bx-home-circle'></i><span>Dashboard</span></a>
                     </li>
-                    <li><a class="dropdown-item" href="javascript:;"><i class='bx bx-dollar-circle'></i><span>Earnings</span></a>
+                    <li><a class="dropdown-item" href="javascript:;"><i class='bx bx-dollar-circle'></i><span>Meus ganhos</span></a>
                     </li>
-                    <li><a class="dropdown-item" href="javascript:;"><i class='bx bx-download'></i><span>Downloads</span></a>
-                    </li>
+                    <!-- <li><a class="dropdown-item" href="javascript:;"><i class='bx bx-download'></i><span>Downloads</span></a>
+                    </li> -->
                     <li>
                         <div class="dropdown-divider mb-0"></div>
                     </li>
